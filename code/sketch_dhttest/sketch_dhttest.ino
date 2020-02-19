4ebf83c79e5e51073c570e73bbffd9b9f8044695
Gavin Rogers <gavinr@skynet.ie>
2020-02-19 12:36:46 +0000

    simplify code...............

--- code/sketch_dhttest/sketch_dhttest.ino
+++ code/sketch_dhttest/sketch_dhttest.ino
@@ -15,6 +15,7 @@ class myinDTemp {
     float _humidity;
     float _temperature;
     DHT* _dht;
+    Adafruit_7segment _matrix = Adafruit_7segment();  
 
   public:
     myinDTemp(byte attachTo) :
@@ -31,6 +32,9 @@ class myinDTemp {
     }
 
     void setup() {
+      _matrix.begin(0x70);
+      _matrix.clear();
+      _matrix.writeDisplay();
       pinMode(_pin, INPUT_PULLUP);
       _dht = new DHT(_pin, _dht_type);
       _dht->begin();
@@ -39,6 +43,37 @@ class myinDTemp {
     void loop() {
       _humidity = _dht->readHumidity();
       _temperature = _dht->readTemperature();
+      display();
+    }
+
+    void display() {
+      // hum
+      _matrix.clear();
+                                     // 0b0001000001110000,
+      // AAAAA
+      // B   C
+      // DDDDD
+      // E   F
+      // GGGGG
+      //_matrix.writeDigitRaw(1, (uint8_t)0b.BDEGFCA);
+      _matrix.writeDigitRaw(1,   (uint8_t)0b01110100);
+      _matrix.writeDigitRaw(2, (uint8_t)0b11111111);
+      _matrix.writeDisplay();
+      delay(1500);
+      _matrix.clear();
+      _matrix.print(_humidity);
+      _matrix.writeDisplay();
+      delay(3500);
+
+      // temp
+      _matrix.clear();
+      _matrix.writeDigitRaw(1, (uint8_t)0b01111000);
+      _matrix.writeDigitRaw(2, (uint8_t)0b11111111);
+      _matrix.writeDisplay();
+      delay(1500);
+      _matrix.clear();
+      _matrix.print(_temperature);
+      _matrix.writeDisplay();
     }
 
     void debug() {
@@ -47,138 +82,23 @@ class myinDTemp {
     }
 };
 
-/*---------------------------------------*/
-class myoutSevenword {
-    Adafruit_AlphaNum4 _matrix = Adafruit_AlphaNum4();
-
-  public:
-    myoutSevenword()
-    {
-    }
-
-    ~myoutSevenword()
-    {
-    }
-
-    void setup() {
-      _matrix.begin(0x70);
-    }
-
-    void loop() {
-      _matrix.setBrightness(0);
-      _matrix.clear();
-      _matrix.writeDigitRaw(0, 0xFFFF); // 8
-      _matrix.writeDigitRaw(4, 0xFFFF); // 8
-      _matrix.writeDisplay();
-      delay(1500);
-      //
-      _matrix.writeDigitAscii(0, 't');
-      _matrix.writeDigitAscii(1, 'E');
-      _matrix.writeDigitAscii(2, ' ');
-      _matrix.writeDigitAscii(3, 'M');
-      _matrix.writeDigitAscii(4, 'P');
-      _matrix.writeDisplay();
-      delay(1500);
-    }
-
-    void debug() {
-      Serial.print("7segword");
-    }
-};
-class myoutSevennum {
-    Adafruit_7segment _matrix = Adafruit_7segment();
-
-  public:
-    myoutSevennum()
-    {
-    }
-
-    ~myoutSevennum()
-    {
-    }
-
-    void setup() {
-      _matrix.begin(0x70);
-    }
-
-    void loop() {
-      _matrix.print(1000, DEC);
-      _matrix.writeDisplay();
-      delay(1500);
-      _matrix.print(0xBEEF, HEX);
-      _matrix.writeDisplay();
-      delay(1500);
-    }
-
-    void debug() {
-      Serial.print("7seg");
-    }
-};
-
-/*---------------------------------------*/
-class myinButton {
-    const byte _pin;
-    int _state;
-    unsigned long _buttonDownMs;
-
-  public:
-    myinButton(byte attachTo) :
-      _pin(attachTo)
-    {
-    }
-
-
-    void setup() {
-      pinMode(_pin, INPUT_PULLUP);
-      _state = HIGH;
-    }
-
-    void loop() {
-      int prevState = _state;
-      _state = digitalRead(_pin);
-      if (prevState == HIGH && _state == LOW) {
-        _buttonDownMs = millis();
-      }
-      else if (prevState == LOW && _state == HIGH) {
-        if (millis() - _buttonDownMs < 50) {
-          // ignore this for debounce
-        }
-        else if (millis() - _buttonDownMs < 250) {
-          // short click
-        }
-        else  {
-          // long click
-        }
-      }
-    }
-};
-
 /*---------------------------------------
 * My vars
 *---------------------------------------*/
-myinButton button(7);
 myinDTemp dtemp(8);
-//myoutSevennum seven;
-myoutSevenword sevenword;
 
 /*---------------------------------------
 * Code
 *---------------------------------------*/
 void setup() {
   Serial.begin(9600);
-  button.setup();
+  
   dtemp.setup();
-  //seven.setup();
-  sevenword.setup();
 }
 
 void loop() {
-  button.loop();
+ 
   dtemp.loop();
   dtemp.debug();
-  //seven.loop();
-  //seven.debug();
-  sevenword.loop();
-  sevenword.debug();
-  delay(1000); //Delay 2 sec. 
+  delay(5000); //Delay 2 sec. 
 }
